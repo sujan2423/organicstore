ede96f2a9019466e59ed4a998d060a97da08e66d
sujan2423 <sujanrokaya92@gmail.com>
2024-09-22 20:13:44 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// document.getElementById("price").innerHTML="$0.00";
+
+// function shownavbar(){
+//     document.querySelector("nav div ul").style.display="block";
+    
+// }
+    
+//var hhamburg = document.getElementById("bars");
+//hhamburg.classList.add("sujan");
+//console.log(hamburg.classlist);
+
+
+
+function shownavbar(){
+    var list =document.querySelector(".mobile");
+    //console.log(list.classlist);
+    list.classList.toggle("list");
+    
+}
